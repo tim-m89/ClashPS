9cb63d2353857d61a2af9a824c3747c54e609ba2
Tim Matthews <tim.matthews7@gmail.com>
2016-01-06 15:27:55 +1300

    Added some documentation

--- src/TimPS.purs
+++ src/TimPS.purs
@@ -11,23 +11,27 @@ import Data.Array(head, filter)
 import Data.Maybe (Maybe(..))
 import Prelude
 
+-- Our player info
 myInfo :: PlayerInfo
 myInfo = { style: 1
          , name: "TimPS" }
 
+-- Our player
 playerTim_ :: Player
 playerTim_ = { ai:   myAi
              , info: myInfo }
 
+-- Our player converted to the native interface without curried functions and constructors
 playerTim :: ClashPrim.Player
 playerTim = toPrimPlayer playerTim_
 
-
+-- Main ai function
 myAi :: Ai
 myAi self enemies game = if hasAmmo self && pointingAtPlayers self enemies
                           then Shoot
                           else myNoShootMove self enemies game
 
+-- Move to be cacrried out when theres no point in shooting on this round
 myNoShootMove :: Ai
 myNoShootMove self enemies game = case turnToInlineEnemiesCurrent self enemies game of
                                     (Just m)  -> m
@@ -51,16 +55,19 @@ turnToInlineEnemiesCurrent = turnToInlineEnemies playerPos
 turnToInlineEnemiesImminent :: PlayerState -> EnemiesState -> GameEnvironment -> Maybe Move
 turnToInlineEnemiesImminent = turnToInlineEnemies playerNextPos
 
+-- Move forward if it will get the ship closer to the closest ammo, otherwise turn if reguired for a move to the closest ammo, otherwise noop
 seekAmmo :: Ai
 seekAmmo self enemies game = case closestPos (playerPos self) (ammoPos game) of
                               (Just pos) -> moveToPos pos self enemies game
                               (Nothing)  -> NoOp
 
+-- Attemp to go forward if it will get us closer to supplied position, otherwise turn for it
 moveToPos :: Position -> Ai
 moveToPos pos self enemies game = if playerEnRouteForPos self pos
                                     then Move
                                     else turnRouteForPos self pos
 
+-- If supplied position is north of us, turn north, etc for each direction
 turnRouteForPos :: PlayerState -> Position -> Move
 turnRouteForPos self pos = let myPos = playerPos self in
                             if      positionIsNorth pos myPos then Turn North
@@ -69,6 +76,7 @@ turnRouteForPos self pos = let myPos = playerPos self in
                             else if positionIsWest  pos myPos then Turn West
                             else NoOp
 
+-- A main entry point to keep the compiler happy
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Loading TimPS"
