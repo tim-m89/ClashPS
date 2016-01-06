509f503bd7a4d02e4b769ee57e6a9a9b30c05561
Tim Matthews <tim.matthews7@gmail.com>
2016-01-06 16:41:40 +1300

    Cleanup Maybe chaining

--- src/TimPS.purs
+++ src/TimPS.purs
@@ -7,9 +7,13 @@ import Clash
 import qualified ClashPrim as ClashPrim
 import ClashPrimConversion (toPrimPlayer)
 import ClashUtil
+import Control.Alt
 import Data.Array(head, filter)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), fromMaybe)
 import Prelude
+
+-- Like Clash.Ai, except the Move is wrapped in a Maybe
+type AiMaybe = PlayerState -> EnemiesState -> GameEnvironment -> Maybe Move
 
 -- Our player info
 myInfo :: PlayerInfo
@@ -33,33 +37,29 @@ myAi self enemies game = if hasAmmo self && pointingAtPlayers self enemies
 
 -- Move to be cacrried out when theres no point in shooting on this round
 myNoShootMove :: Ai
-myNoShootMove self enemies game = case turnToInlineEnemiesCurrent self enemies game of
-                                    (Just m)  -> m
-                                    (Nothing) -> case turnToInlineEnemiesImminent self enemies game of
-                                      (Just m)  -> m
-                                      (Nothing) -> seekAmmo self enemies game
+myNoShootMove self enemies game = fromMaybe NoOp $  turnToInlineEnemiesCurrent self enemies game
+                                                <|> turnToInlineEnemiesImminent self enemies game
+                                                <|> seekAmmo self enemies game
 
-turnToInlineEnemies :: (PlayerState -> Position) -> PlayerState -> EnemiesState -> GameEnvironment -> Maybe Move
+turnToInlineEnemies :: (PlayerState -> Position) -> AiMaybe
 turnToInlineEnemies fPlayerPos self enemies game = if not (hasAmmo self)
-                                          then Nothing
-                                          else let myPos      = playerPos self
-                                                   enemiesPos = map fPlayerPos enemies
-                                                   inline     = filter (\pos-> posInline myPos pos) enemiesPos
-                                                    in head inline >>= \pos-> return $ turnRouteForPos self pos
+                                                    then Nothing
+                                                    else let myPos      = playerPos self
+                                                             enemiesPos = map fPlayerPos enemies
+                                                             inline     = filter (\pos-> posInline myPos pos) enemiesPos
+                                                              in head inline >>= \pos-> return $ turnRouteForPos self pos
 
 -- Turn to enemies that are currently inline with self
-turnToInlineEnemiesCurrent :: PlayerState -> EnemiesState -> GameEnvironment -> Maybe Move
+turnToInlineEnemiesCurrent :: AiMaybe
 turnToInlineEnemiesCurrent = turnToInlineEnemies playerPos
 
 -- Turn to where an enemy is about to be inline if they are to go straight ahead on their next move
-turnToInlineEnemiesImminent :: PlayerState -> EnemiesState -> GameEnvironment -> Maybe Move
+turnToInlineEnemiesImminent :: AiMaybe
 turnToInlineEnemiesImminent = turnToInlineEnemies playerNextPos
 
 -- Move forward if it will get the ship closer to the closest ammo, otherwise turn if reguired for a move to the closest ammo, otherwise noop
-seekAmmo :: Ai
-seekAmmo self enemies game = case closestPos (playerPos self) (ammoPos game) of
-                              (Just pos) -> moveToPos pos self enemies game
-                              (Nothing)  -> NoOp
+seekAmmo :: AiMaybe
+seekAmmo self enemies game = closestPos (playerPos self) (ammoPos game) >>= \pos-> return $ moveToPos pos self enemies game
 
 -- Attemp to go forward if it will get us closer to supplied position, otherwise turn for it
 moveToPos :: Position -> Ai
